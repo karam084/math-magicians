7679fe418a406e3ecbe4bf4945cb80db894c3e7d
karam084 <karam.ezz84@gmail.com>
2022-08-05 21:25:52 +0200

    Fix stop linter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Calculator from './components/Calculator';
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
