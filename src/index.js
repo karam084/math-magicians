025e9f6cf6e7b15502dbdaac5c2bada2c26d43fe
karam084 <karam.ezz84@gmail.com>
2022-08-10 23:33:48 +0200

    Inital full website

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import {
+  BrowserRouter as Router, Routes,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/app" element={<App />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </Router>,
+  document.getElementById('root'),
 );
-
